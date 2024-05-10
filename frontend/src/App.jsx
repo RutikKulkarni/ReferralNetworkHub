9f28f9ca221398ceb6162b126a94ad5ec16f0369
AbuQamar7379 <shaikhqamar7379@gmail.com>
2024-05-10 09:38:56 +0530

    Navbar complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import LandingPage from "./components/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Navbar/Navbar";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default LandingPage;

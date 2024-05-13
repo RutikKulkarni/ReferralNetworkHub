52191625cf114295f57bcad9d653eeee3eb8f5a2
AbuQamar7379 <shaikhqamar7379@gmail.com>
2024-05-13 18:59:04 +0530

    Implement Hero section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import styles from "./Home.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import Hero from "../../components/Hero/Hero";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className={styles.hero}>
-        <div className={styles.heroContent}>
-          <h1>Welcome to Our Website</h1>
-          <p>Discover amazing things here!</p>
-          <button className={styles.heroBtn}>Get Started</button>
-        </div>
-      </div>
+      <Hero />
       <Footer />
     </div>
   );

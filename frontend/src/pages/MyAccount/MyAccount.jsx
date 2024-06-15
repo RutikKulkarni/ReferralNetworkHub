be5fce175e2cdb4a061481b77f21ff12650a2f58
Rutik <rutikkulkarni2001@gmail.com>
2024-06-15 15:09:04 +0530

    Profile page added

--- frontend/src/pages/MyAccount/MyAccount.jsx
+++ frontend/src/pages/MyAccount/MyAccount.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import styles from "./MyAccount.module.css";
+import Profile from "../Profile/Profile";
 
 /**
  * Home component representing the Home page.
  * @returns {JSX.Element} Home JSX element
  */
 const MyAccount = () => {
-  const navigate = useNavigate();
-  const handleEditAccountInfo = () => {
-    navigate("/EditAccountInfo");
-  };
   return (
     <div>
       <Navbar />
-        <button onClick={handleEditAccountInfo} style={{ display: 'block', margin: '10px auto' }}>EDIT</button>
+      <Profile />
       <Footer />
     </div>
   );

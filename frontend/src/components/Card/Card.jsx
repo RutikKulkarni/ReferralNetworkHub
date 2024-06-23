272e81a009e96dfff50904ca37976bb683a096c5
somesh pundir <pundirsomesh@gmail.com>
2024-06-23 18:04:36 +0530

    explore page

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import styles from './Card.module.css';
-
+import React from "react";
+import styles from "./Card.module.css";
+import Avtar from '../../assets/png/avatar.png'
 const Card = ({ name, title, company }) => {
   return (
     <div className={styles.card}>
-      <img src="profile-pic-url" alt="profile" className={styles.profilePic} />
-      <div className={styles.details}>
-        <div className={styles.info}>
-          <h2>{name}</h2>
-          <p>{title}</p>
-        </div>
+      <div className={styles.imageWrapper}>
+        <img src={Avtar} alt="profile" />
+      </div>
+
+      <div className={styles.info}>
+        <h2>{name}</h2>
+        <p>{title}</p>
+      </div>
+
+      <div className={styles.companyDetails}>
         {company && <p className={styles.company}>{company}</p>}
       </div>
     </div>
--- frontend/src/pages/Explore/Explore.jsx
+++ frontend/src/pages/Explore/Explore.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
 import styles from './Explore.module.css';
+import { IoArrowForward } from "react-icons/io5";
 
 const Explore = () => {
-  const cardsData = [
+  const cardData = [
     { name: 'John Doe', title: 'Software Engineer - 1', company: 'Company Logo' },
     { name: 'John Doe', title: 'Software Engineer - 1', company: 'Company Logo' },
     { name: 'John Doe', title: 'Software Engineer - 1', company: 'Company Logo' },
   ];
 
   return (
+    <>
     <div className={styles.exploreWrapper}>
-      <div className={styles.explore}>
-        <h1 className={styles.heading}>Explore</h1>
-        <div className={styles.cardContainer}>
-          {cardsData.map((card, index) => (
-            <Card key={index} {...card} />
-          ))}
-        </div>
+      <div className={styles.mainText}>
+        <h1>Explore</h1>
+        <IoArrowForward className={styles.arrowIcon}/>
+      </div>
+      <div className={styles.gridContainer}>
+        {cardData.map((card, index) => (
+          <div key={index} className={styles.gridItem}>
+            <Card {...card} />
+          </div>
+        ))}
       </div>
     </div>
+    </>
   );
 };
 

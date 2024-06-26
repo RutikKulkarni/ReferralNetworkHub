398d192939b48c2c6c854756fa57c4ec73b835a8
Rutik <rutikkulkarni2001@gmail.com>
2024-06-26 21:07:21 +0530

    Update the card data to test the card logic

--- frontend/src/pages/Explore/cardData.js
+++ frontend/src/pages/Explore/cardData.js
@@ -6,9 +6,9 @@ const cardData = [
     { name: 'John Doe', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
     { name: 'rghnrerger Ddfgrgerg tryherherh', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
     { name: 'cbhnrerger sdrgsrgerg kkherherh', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
-    { name: 'Raghavendra Kulkarni', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
-    { name: 'Raghavendra Govind Kulkarni', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
-    { name: 'Rutik Govind Kulkarni', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
+    { name: 'abcdaveabcd abcdabcd', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
+    { name: 'abcdaveabcd asdasd abcdabcd', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
+    { name: 'asdik asdasd abcdabcd', title: 'Software Engineer - 1', company: 'Company Logo', img: "https://via.placeholder.com/50" },
 ];
 
 export default cardData;
